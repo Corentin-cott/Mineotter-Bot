c372a77d35bbfe5c60576ef6cb99d9d9e89cb119
Corentin-cott <corentin.cott@gmail.com>
2025-05-24 22:35:54 +0200

    Ajout du port pour MYSQL

--- src/events/createServerInteraction.ts
+++ src/events/createServerInteraction.ts
@@ -21,6 +21,7 @@ const event: BotEvent = {
             }
             let bot_color: string;
             bot_color = process.env.BOT_COLOR || "#FFFFFF";
+            otterlogs.warn(`Le bot_color est ${bot_color}`);
 
             /* Questions à poser pour la configuration du serveur
             serveur_pack_url?: string;
@@ -34,36 +35,71 @@ const event: BotEvent = {
             */
 
             try {
-                const questionEmbed = new EmbedBuilder()
-                .setTitle("Étape 1/6 : Utilisation d'un serveur pack ?")
-                .setDescription(``)
-                .setColor(bot_color as ColorResolvable)
-                .setFooter({
-                    text: "Mineotter",
-                    iconURL: interaction.client.user?.displayAvatarURL() || '',
-                })
-                .setTimestamp();
+                try {
+                    await interaction.deferReply({ ephemeral: true });
+                } catch (error) {
+                    otterlogs.error(`Erreur lors du "deferReply" : ${error}`);
+                    await interaction.reply({
+                        content: "❌ Une erreur s'est produite en traitant ta demande.",
+                        ephemeral: true,
+                    });
+                    return;
+                }
 
-                const yesButton = new ButtonBuilder()
-                    .setCustomId('yesButton')
-                    .setLabel('Commencer')
-                    .setStyle(ButtonStyle.Success);
-                const noButton = new ButtonBuilder()
-                    .setCustomId('noButton')
-                    .setLabel('Commencer')
-                    .setStyle(ButtonStyle.Danger);
-                const row = new ActionRowBuilder<ButtonBuilder>()
-                    .addComponents(yesButton, noButton);
+                /*
+                let questionEmbed = new EmbedBuilder()
+                let row = new ActionRowBuilder<ButtonBuilder>();
+
+                try {
+                    questionEmbed
+                    .setTitle("Étape 1/6 : Utilisation d'un serveur pack ?")
+                    .setDescription(``)
+                    .setColor(bot_color as ColorResolvable)
+                    .setFooter({
+                        text: "Mineotter",
+                        iconURL: interaction.client.user?.displayAvatarURL() || '',
+                    })
+                    .setTimestamp();
+
+                    const yesButton = new ButtonBuilder()
+                        .setCustomId('yesButton')
+                        .setLabel('Oui')
+                        .setStyle(ButtonStyle.Success);
+                    const noButton = new ButtonBuilder()
+                        .setCustomId('noButton')
+                        .setLabel('No')
+                        .setStyle(ButtonStyle.Danger);
+
+                    row.addComponents(yesButton, noButton);
+                } catch (error) {
+                    otterlogs.error(`Erreur lors de la création du message : ${error}`);
+                    await interaction.editReply({
+                        content: "❌ Une erreur s'est produite en traitant ta demande."
+                    });
+                    return;
+                }
     
-                await channel.send({
-                    embeds: [questionEmbed],
-                    components: [row.toJSON()]
+                try {
+                    await channel.send({
+                        embeds: [questionEmbed],
+                        components: [row.toJSON()]
+                    });
+                } catch (error) {
+                    otterlogs.error(`Erreur lors de l'envoi du message dans le salon de création : ${error}`);
+                    await interaction.editReply({
+                        content: "❌ Une erreur s'est produite en traitant ta demande."
+                    });
+                    return;
+                }
+                */
+
+                await interaction.editReply({
+                    content: "Désolé, pas encore prêt !"
                 });
             } catch (error) {
                 otterlogs.error(`Erreur lors du clic sur le bouton Commencer : ${error}`);
-                await interaction.followUp({
-                    content: "❌ Une erreur s'est produite en traitant ta demande." + error,
-                    ephemeral: true,
+                await interaction.editReply({
+                    content: "❌ Une erreur s'est produite en traitant ta demande." + error
                 });
             }
         }
